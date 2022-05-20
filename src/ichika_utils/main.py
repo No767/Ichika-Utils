af25b8e22e5a78c7bc0e8f77499e0defad8f4f32
No767 <73260931+No767@users.noreply.github.com>
2022-05-19 23:42:44 -0700

    Make sure that versions are working

--- src/ichika_utils/main.py
+++ src/ichika_utils/main.py
@@ -30,64 +30,35 @@ class IchikaUtils:
         
     async def graphHistoRandn(self, amount: int):
         fig, ax = plt.subplots()
-
-# Fixing random state for reproducibility
         np.random.seed(19680801)
-
-
-# histogram our data with numpy
         n, bins = np.histogram(amount, 50)
-        
         left = bins[:-1]
         right = bins[1:]
         bottom = np.zeros(len(left))
         top = bottom + n
-
-
-        # we need a (numrects x numsides x 2) numpy array for the path helper
-        # function to build a compound path
         XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
-
-        # get the Path object
         barpath = path.Path.make_compound_path_from_polys(XY)
-
-        # make a patch out of it
         patch = patches.PathPatch(barpath)
         ax.add_patch(patch)
-
-        # update the view limits
         ax.set_xlim(left[0], right[-1])
         ax.set_ylim(bottom.min(), top.max())
-
         plt.show()
+        
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
     async def graphSetHistogram(self, data: list):
-# for one dimensional data
         fig, ax = plt.subplots()
-
         (hist, bin_edges) = np.histogram(data)
         left = bin_edges[:-1]
         right = bin_edges[1:]
         bottom = np.zeros(len(left))
         top = bottom + hist
-
-
-        # we need a (numrects x numsides x 2) numpy array for the path helper
-        # function to build a compound path
         XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
-
-        # get the Path object
         barpath = path.Path.make_compound_path_from_polys(XY)
-
-        # make a patch out of it
         patch = patches.PathPatch(barpath)
         ax.add_patch(patch)
-
-        # update the view limits
         ax.set_xlim(left[0], right[-1])
         ax.set_ylim(bottom.min(), top.max())
-
         plt.show()
 
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())        
